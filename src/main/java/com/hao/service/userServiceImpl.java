87715e7a0a9e4c39006f783299eb4ffc1633f161
yaoyao <3475839227@qq.com>
2022-08-01 21:29:01 +0800

    更改

--- src/main/java/com/hao/service/userServiceImpl.java
+++ src/main/java/com/hao/service/userServiceImpl.java
@@ -40,7 +40,7 @@ public class userServiceImpl implements userService{
         //发送    https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code 获取用户的openid和session_key
         //注意这个是 WeChatTool.wxspAppid 是微信小程序的appid 从微信小程序后台获取 WeChatTool.wxspSecret 这个也一样，我这里是用了常量来进行保存方便多次使用
         String url="https://api.weixin.qq.com/sns/jscode2session";
-        String params = "appid=wxe35781703e6544b7" + "&secret=" + "8e0c1ee4a65b34b16905083f4b040442" + "&js_code=" + code + "&grant_type=authorization_code";
+        String params = "appid=wxe35781703e6544b7" + "&secret=" + "" + "&js_code=" + code + "&grant_type=authorization_code";
         String sendGet = Httprequests.sendGet(url, params); //发起请求拿到key和openid
         return sendGet;
 
